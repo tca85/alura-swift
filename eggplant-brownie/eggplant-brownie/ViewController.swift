46e4696d3502671a570d6f3af4e418469fd0419e
Thiago C. Alves <thiago.alves.85@gmail.com>
2017-08-15 20:08:08 -0400

    creating a new screen and a pushbutton action

--- eggplant-brownie/eggplant-brownie/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -10,16 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func add() {
+        print("adding a new meal")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
